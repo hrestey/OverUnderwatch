376963ceb90ac430e98a10a97d6a707d54f42bd6
Hayden Estey <hestey@stetson.edu>
2018-02-26 14:56:07 -0500

    Updated compareRecords to assume that it is only being called at the end of the week. Added a test for a tie, and added a test to call the main predicates if its a "less than" call. Added a predicate to check if it's the end of the stage (stubbed out), and added a predicate to represent a match (old pred changed to matchScore).

--- stage-structure.pl
+++ stage-structure.pl
@@ -1,12 +1,14 @@
 :- [teams].
 
-match(Winner, Team1, Team2, MapScore).
+matchScore(Winner, Team1, Team2, MapScore).
+
+match(matchScore(Winner, Team1, Team2, MapScore), Stage, Week).
 
 unplayedMatch(Team1, Team2).
 
 schedule(Matches).
 
-record(Team, NumberofGamesPlayed, Wins, Losses, MapDifferential).
+record(Team, Wins, Losses, MapDifferential).
 
 headToHeadMapDiff(Team1, Team2, HeadToHeadOwner).
 headToHeadMapDiff(Team1, Team2, tie).
@@ -16,55 +18,43 @@ headToHead(Team1, Team2, tie).
 
 tieBreakerMatch(Team1, Team2, Winner).
 
+endOfStage(Wins, Losses). % checks if the Wins and Losses added together is evenly divisible by 10 (therefore end of stage)
+
 compareRecords(>, Record1, Record2) :-
-    record(team(Team1), NoG1, W1, L1, MD1), record(team(Team2), NoG2, W2, L2, MD2),
-    NoG1 = NoG2,
+    record(team(Team1), W1, L1, MD1), record(team(Team2), W2, L2, MD2),
     W1 > W2.
 compareRecords(>, Record1, Record2) :-
-    record(team(Team1), NoG1, W1, L1, MD1), record(team(Team2), NoG2, W2, L2, MD2),
-    NoG1 = NoG2,
+    record(team(Team1), W1, L1, MD1), record(team(Team2), W2, L2, MD2),
     W1 = W2,
     MD1 > MD2.
 compareRecords(>, Record1, Record2) :-
-    record(team(Team1), NoG1, W1, L1, MD1), record(team(Team2), NoG2, W2, L2, MD2),
-    NoG1 = NoG2,
+    record(team(Team1), W1, L1, MD1), record(team(Team2), W2, L2, MD2),
     W1 = W2,
     MD1 = MD2,
     headToHeadMapDiff(Team1, Team2, Team1). % Head to Head Map Differential in favor of Team1
 compareRecords(>, Record1, Record2) :-
-    record(team(Team1), NoG1, W1, L1, MD1), record(team(Team2), NoG2, W2, L2, MD2),
-    NoG1 = NoG2,
+    record(team(Team1), W1, L1, MD1), record(team(Team2), W2, L2, MD2),
     W1 = W2,
     MD1 = MD2,
     headToHeadMapDiff(Team1, Team2, tie),
     headToHead(Team1, Team2, Team1).
-compareRecords(>, Record1, Record2) :-
-    record(team(Team1(, NoG1, W1, L1, MD1), record(team(Team2), NoG2, W2, L2, MD2),
-    NoG1 = NoG2,
+compareRecords(>, Record1, Record2) :- % should be restricted to only at the end of stages
+    record(team(Team1), W1, L1, MD1), record(team(Team2), W2, L2, MD2),
     W1 = W2,
     MD1 = MD2,
     headToHead(MapDiff(Team1, Team2, tie),
     headToHead(Team1, Team2, tie),
+    endOfStage(W1, L1),
     tieBreakerMatch(Team1, Team2, Team1).
-compareRecords(>, Record1, Record2) :-
-    record(team(Team1), NoG1, W1, L1, MD1), record(team(Team2), NoG2, W2, L2, MD2),
-    NoG1 \= NoG2,
-    W1 > W2,
-    L1 < L2.
-compareRecords(>, Record1, Record2) :-
-    record(team(Team1), NoG1, W1, L1, MD1), record(team(Team2), NoG2, W2, L2, MD2),
-    NoG1 \= NoG2,
-    W1 > W2,
-    L1 = L2.
-compareRecords(>, Record1, Record2) :-
-    record(team(Team1), NoG1, W1, L1, MD1), record(team(Team2), NoG2, W2, L2, MD2),
-    NoG1 \= NoG2,
+compareRecords(=, Record1, Record2) :-
+    record(team(Team1), W1, L1, MD1), record(team(Team2), W2, L2, MD2),
     W1 = W2,
-    L1 < L2.
-compareRecords(>, Record1, Record2) :-
-    record(team(Team1), NoG1, W1, L1, MD1), record(team(Team2), NoG2, W2, L2, MD2),
-    NoG1 \= NoG2,
-    W1 = W2, 
+    MD1 = MD2,
+    headToHeadMadDiff(Team1, Team2, tie),
+    headToHead(Team1, Team2, tie),
+    /=endOfStage(W1, L1).
+compareRecords(<, Team1, Team2) :-
+    compareRecords(>, Team2, Team1).
 
 teamStandings([], [_]).
 teamStandings(Records, Standings, FinalStandings) :-
