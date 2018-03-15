7c79a7dbedaca7f04a18416a6fc8bb66b3fb9f1f
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2018-03-15 16:21:50 -0400

    Fixed recursive issues, added team rankings (1st, 2nd, ...)

--- stage-structure.pl
+++ stage-structure.pl
@@ -22,16 +22,22 @@ headToHead(Team1, Team2, tie).
 
 tieBreakerMatch(Team1, Team2, Winner).*/
 
+% the two teams (first two values) must be alphabetically sorted
 headToHeadMapDiff(dallas, seoul, seoul).
 headToHeadMapDiff(houston, london, tie).
-headToHeadMapDiff(florida, dallas, tie).
-headToHeadMapDiff(X, Y, X) :- headToHeadMapDiff(Y, X, X), !.
-headToHeadMapDiff(X, Y, tie) :- headToHeadMapDiff(Y, X, tie), !.
+headToHeadMapDiff(dallas, florida, tie).
+headToHeadMapDiff(dallas, newyork, tie).
+% if called out-of-order (alphabetically), flip
+headToHeadMapDiff(X, Y, X) :- X @> Y, headToHeadMapDiff(Y, X, X).
+headToHeadMapDiff(X, Y, tie) :- X @> Y, headToHeadMapDiff(Y, X, tie).
 
+% the two teams (first two values) must be alphabetically sorted
 headToHead(dallas, florida, dallas).
 headToHead(houston, london, tie).
-headToHead(X, Y, X) :- headToHead(Y, X, X), !.
-headToHead(X, Y, tie) :- headToHead(Y, X, tie), !.
+headToHead(dallas, newyork, tie).
+% if called out-of-order (alphabetically), flip
+headToHead(X, Y, X) :- X @> Y, headToHead(Y, X, X).
+headToHead(X, Y, tie) :- X @> Y, headToHead(Y, X, tie).
 
 endOfStage(Wins, Losses) :-
     Sum is Wins + Losses,
@@ -69,19 +75,31 @@ compareRecords(>, Record1, Record2) :- % should be restricted to only at the end
     headToHead(Team1, Team2, tie),
     endOfStage(W1, L1),
     tieBreakerMatch(Team1, Team2, Team1).
-compareRecords(=, Record1, Record2) :-
+% tie case
+compareRecords(>, Record1, Record2) :-
     Record1 = record(team(Team1), W1, L1, MD1),
     Record2 = record(team(Team2), W2, _, MD2),
     W1 = W2,
     MD1 = MD2,
     headToHeadMapDiff(Team1, Team2, tie),
     headToHead(Team1, Team2, tie),
-    \+endOfStage(W1, L1).
+    \+endOfStage(W1, L1),
+    % ensure alphabetical order of team names
+    Team1 @> Team2.
 
 compareRecords(<, Team1, Team2) :-
     compareRecords(>, Team2, Team1).
 
+% label teams as 1st, 2nd, etc.
+assignStandings(_, [], []).
+assignStandings(N, [H|T], [(N, H)|Rest]) :-
+    N2 is N + 1,
+    assignStandings(N2, T, Rest).
+
 teamStandings(Records, Standings) :-
-    predsort(compareRecords, Records, Standings).
+    predsort(compareRecords, Records, SortedRecords),
+    % group teams by their records, so we can establish standings
+    findall(Teams, bagof(Team, member(record(Team, _, _, _), SortedRecords), Teams), GroupedRecords),
+    assignStandings(1, GroupedRecords, Standings).
 
 
